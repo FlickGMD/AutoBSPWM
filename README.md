e89552a3ac578ef5e6e80a8333bb2b00e1391945
FlickGMD <salvadorfabiansulcacuba@gmail.com>
2024-12-01 22:41:08 -0500

    Probando

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# AutoBSPWM
+### **¿Cómo usar?**
+
+```
+cd AutoBSPWM
+chmod +x desktop.sh
+./desktop.sh
+```
+
+------------------------------------------------------------------------
+
+### **Atajos de teclado**
+- **`Windows + Enter -> /opt/kitty/bin/kitty`**  
+- **`Windows + Shift + O -> /usr/bin/obsidian`**  
+- **`Windows + Shift + X -> /usr/bin/i3lock-fancy`**  
+- **`Windows + D -> ~/.config/rofi/launchers/type-6/launcher.sh`**  
+- **`Windows + Escape -> pkill -USR1 -x sxhkd`**  
+- **`Windows + Shift + F -> /usr/bin/firefox`**  
+- **`Windows + Shift + {Q,R} -> bspc {quit,wm -r}`**  
+- **`Windows + {Q,Shift + Q} -> bspc node -{c,k}`**  
+- **`Windows + M -> bspc desktop -l next`**  
+- **`Windows + Y -> bspc node newest.marked.local -n newest.!automatic.local`**  
+- **`Windows + G -> bspc node -s biggest.window`**  
+- **`Windows + {T,Shift + T,S,F} -> bspc node -t {tiled,pseudo_tiled,floating,fullscreen}`**  
+- **`Windows + Ctrl + {M,X,Y,Z} -> bspc node -g {marked,locked,sticky,private}`**  
+- **`Windows + {Left,Down,Up,Right} -> bspc node -f {west,south,north,east}`**  
+- **`Windows + Shift + {Left,Down,Up,Right} -> bspc node -s {west,south,north,east}`**  
+- **`Windows + {P,B,Comma,Period} -> bspc node -f @{parent,brother,first,second}`**  
+- **`Windows + {C,Shift + C} -> bspc node -f {next,prev}.local.!hidden.window`**  
+- **`Windows + Bracket {Left,Right} -> bspc desktop -f {prev,next}.local`**  
+- **`Windows + {Grave,Tab} -> bspc {node,desktop} -f last`**  
+- **`Windows + {O,I} -> bspc wm -h off; bspc node {older,newer} -f; bspc wm -h on`**  
+- **`Windows + {1-9,Shift + 1-9} -> bspc {desktop -f,node -d} ^{1-9}`**  
+- **`Windows + Ctrl + Alt + {Left,Down,Up,Right} -> bspc node -p {west,south,north,east}`**  
+- **`Windows + Ctrl + {1-9} -> bspc node -o 0.{1-9}`**  
+- **`Windows + Ctrl + Alt + Space -> bspc node -p cancel`**  
+- **`Windows + Ctrl + Shift + Space -> bspc query -N -d | xargs -I id -n 1 bspc node id -p cancel`**  
+- **`Windows + Shift + {Left,Down,Up,Right} -> bspc node -v {-20 0,0 20,0 -20,20 0}`**  
+- **`Windows + Alt + {Left,Down,Up,Right} >/home/kali/.config/bspwm/scripts/bspwm_resize {west,south,north,east}`**  
+- **`Windows + a -> flameshot gui -p /home/kali/Imágenes/capturas/$(date +"%Y-%m-%d_%H-%M-%S").png`**  
+
+**Esto es una prueba y no debes ejecutar el script :)**
---
+++ desktop.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+if [[ $(whoami) == "root" ]] || [[ $(id -u) -eq 0 ]]; then
+  echo -e "\n[!] Este script no puede ser ejecutado como root"
+  echo -e "Ejecutar: exit"
+  echo -e "Ejecutar ./$0"
+  exit 1
+fi
+
+function handler(){
+  echo -e "\n[!] Saliendo..."
+  exit 1
+}
+trap handler INT
+RUTA=$(pwd)
+# ACTUALIZAMOS EL PROGRAMA EN BASE A NUESTRO SISTEMA OPERATIVO
+if [[ $(hostname) == *"kali"* ]]; then
+  echo -e "\n[*] Actualizando sistema kali."
+  sudo apt update
+  sudo apt full-upgrade -y 
+  echo -e "\n[*] Actualizacion finalizada."
+elif [[ $(hostname) == *"parrot"* ]]; then
+  echo -e "\n[*] Actualizando sistema kali."
+  sudo apt update
+  sudo parrot-upgrade -y
+  echo -e "\n[*] Actualizacion finalizada."
+else
+  echo -e "\n[!] $(hostname): NO ENCONTRADO."
+  exit 1
+fi
+
+# INSTALANDO LOS PRINCIPALES 
+sudo apt install bspwm sxhkd polybar rofi -y
+sudo apt-get install libxcb-randr0-dev libxcb-xtest0-dev libxcb-xinerama0-dev libxcb-shape0-dev libxcb-xkb-dev libpcre3-dev -y 
+sudo apt install xcb -y
+sudo apt  install build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-de -y
+sudo apt install -y python3-venv
+sudo apt install glxinfo
+# INSTALANDO BSPWM Y SU REPO
+
+# PICOM INSTALACION
+echo -e "\n[*] Obteniendo picom"
+git clone https://github.com/ibhagwan/picom.git
+
+# DEPENDENCIAS DE MESSON
+sudo apt install meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre3-dev libpcre3 libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev libev-dev -y
+
+cd picom
+git submodule update --init --recursive
+meson --buildtype=release . build
+ninja -C build
+sudo ninja -C build install
+
+# FINALIZAMOS LA INSTALACION DE PICOM
+
+# ULTIMA VERSIÓN DE KITTY A INSTALAR
+kitty=$(7z l kitty-0.37.0-arm64.txz | tail -n 3 | head -n 1 | awk 'NF{print $NF}')
+cd kitty
+7z x kitty-0.37.0-arm64.txz
+sudo tar -xf $kitty
+cd ..
+# INSTALACIÓN DE NVIM
+nvim=$(7z l nvim-linux64.tar.gz | tail -n 3 | head -n 1 | awk 'NF{print $NF}')
+cd PaquetesNvim
+7z x nvim-linux64.tar.gz 
+sudo tar -xf $nvim
+
+python3 -m venv .venv
+source .venv/bin/activate
+.venv/bin/pip3 install pywal
+
+#-----------------------------#
+# FIX THE NVIM PROBLEM        #
+#-----------------------------#
+#pylint fails to install: python3 failed with exit code 1 and signal 0.
+sudo apt install -y python3-venv
